2b2b14bad736bc6fb4608810f0d1188f21131216
Bárbara Cancino <barbaracancino@live.cl>
2023-10-07 12:41:35 -0300

    Corrección de escala de gráfico

--- graphic_calculator.py
+++ graphic_calculator.py
@@ -3,45 +3,55 @@ from math import *
 
 
 def drawgraph(TA, TB):
-    """
-    plotPoints_A = []
-    plotPoints_B = []
-
-    for x in range(0, TA[0] + 1):
-        y = TA[0] - TA[1] * x  # equation goes here
-        y = -y + 100
-        plotPoints_A.append([x, y])
-
-    for x in range(0, TB[0] + 1):
-        y = TB[0] - TB[1] * x  # equation goes here
-        y = -y + 100
-        plotPoints_B.append([x, y])
-    """
     pygame.init()
     screen = pygame.display.set_mode([800, 800])
     screen.fill([255, 255, 255])
     # Dibujamos los ejes del gráfico
     pygame.draw.line(
-        screen, [0, 0, 0], start_pos=[100, 100], end_pos=[100, 700], width=2
+        screen, [0, 0, 0], start_pos=[100, 100], end_pos=[100, 700], width=4
     )
     pygame.draw.line(
-        screen, [0, 0, 0], start_pos=[100, 700], end_pos=[700, 700], width=2
+        screen, [0, 0, 0], start_pos=[100, 700], end_pos=[700, 700], width=4
     )
     # Dibujamos las curvas de demanda
+
+    TA_pos = [[100, 700 - (TA[0] / TA[1])], [100 + TA[0], 700]]
+    TB_pos = [[100, 700 - (TB[0] / TB[1])], [100 + TB[0], 700]]
+
+    max_disp_pagar_A = TA[0] / TA[1]
+    max_disp_pagar_B = TB[0] / TB[1]
+
+    if max_disp_pagar_A == max_disp_pagar_B:
+        TA_pos[0][1] = 200
+        TB_pos[0][1] = 200
+    elif max_disp_pagar_A > max_disp_pagar_B:
+        TA_pos[0][1] = 200
+        TB_pos[0][1] = (max_disp_pagar_B / max_disp_pagar_A) * 600
+    else:
+        TB_pos[0][1] = 200
+        TA_pos[0][1] = (max_disp_pagar_A / max_disp_pagar_B) * 600
+
+    max_q_A = TA[0]
+    max_q_B = TB[0]
+
+    if max_q_A == max_q_B:
+        TA_pos[1][0] = 600
+        TB_pos[1][0] = 600
+    elif max_q_A > max_q_B:
+        TA_pos[1][0] = 600
+        TB_pos[1][0] = 100 + ((max_q_B / max_q_A) * 600)
+    else:
+        TA_pos[1][0] = 100 + ((max_q_A / max_q_B) * 600)
+        TB_pos[1][0] = 600
+
+    # Función de demanda de clientes de demanda alta
     pygame.draw.line(
-        screen,
-        [255, 0, 0],
-        start_pos=[100, 700 - (TA[0] / TA[1])],
-        end_pos=[100 + TA[0], 700],
+        screen, [255, 0, 0], start_pos=TA_pos[0], end_pos=TA_pos[1], width=4
     )
+    # Función de demanda de clientes de demanda baja
     pygame.draw.line(
-        screen,
-        [0, 0, 255],
-        start_pos=[100, 700 - (TB[0] / TB[1])],
-        end_pos=[100 + TB[0], 700],
+        screen, [0, 0, 255], start_pos=TB_pos[0], end_pos=TB_pos[1], width=4
     )
-    # pygame.draw.lines(screen, [255, 0, 0], False, plotPoints_A, 2)
-    # pygame.draw.lines(screen, [0, 0, 255], False, plotPoints_B, 2)
     pygame.display.flip()
 
 
