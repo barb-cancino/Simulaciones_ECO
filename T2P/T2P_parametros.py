d533eb16926d3ec3272cd5d2f24bc5a67b39500f
Bárbara Cancino <barbaracancino@live.cl>
2023-10-12 16:32:39 -0300

    Se incorpora documentación a las funciones definidas

--- T2P/T2P_parametros.py
+++ T2P/T2P_parametros.py
@@ -1,18 +1,26 @@
+"""Se definen parámetros del problema"""
+
+
 def t1():
+    """Parámetros de la función de demanda de un cliente tipo 1"""
     return [11, 1]
 
 
 def t2():
+    """Parámetros de la función de demanda de un cliente tipo 2"""
     return [8.5, 0.5]
 
 
-def CMg():
+def cmg():
+    """Valor del costo marginal constante de la función de costos"""
     return 1
 
 
 def alpha():
+    """Proporción de clientes de demanda alta"""
     return 0.5
 
 
 def n():
+    """Número de clientes en el mercado"""
     return 200
