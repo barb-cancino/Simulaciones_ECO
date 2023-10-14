cb4b98af0f93829e691355879a8820a661b38a9d
Bárbara Cancino <barbaracancino@live.cl>
2023-10-14 17:38:58 -0300

    Elimina código erroneo

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/static")
-def style():
-    return url_for("static", filename="style.css")
-
-
 if __name__ == "__main__":
     app.run()
