1dd299ebdadb09717824be182d7d9d4640388b9d
Bárbara Cancino <barbaracancino@live.cl>
2023-10-15 01:31:45 -0300

    Creación de link a página con resultados

--- views.py
+++ views.py
@@ -6,3 +6,8 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def t2p():
     return render_template("T2P.html")
+
+
+@views.route("/results")
+def results():
+    return render_template("results.html", benefits="3000")
