04d007ab1587e39091181de861a42484b3e9fe28
Bárbara Cancino <barbaracancino@live.cl>
2023-10-27 14:20:32 -0300

    Creación de ejes y curvas de demandas estáticas

--- t2p_graph.js
+++ t2p_graph.js
@@ -1,19 +1,66 @@
 
 const canvas = document.getElementById("t2p_graph");
+
+const heigthUnit= canvas.clientHeight/8
+const widthUnit = canvas.clientWidth / 8
+const heigthSubUnit = heigthUnit/8
+const widthSubUnit = widthUnit/8
+
 if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
+    // Ejes
+    ctx.lineWidth = 2
+    ctx.beginPath();
+    ctx.moveTo(widthUnit, heigthUnit);
+    ctx.lineTo(widthUnit, heigthUnit * 7);
+    ctx.lineTo(widthUnit * 7, heigthUnit * 7);
+    ctx.stroke();
 
-    ctx.fillStyle = "rgb(200, 0, 0)";
-    ctx.fillRect(10, 10, 50, 50);
-
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect(30, 30, 50, 50);
+    // flechas ejes
+    ctx.beginPath();
+    ctx.moveTo(widthUnit - widthSubUnit, heigthUnit);
+    ctx.lineTo(widthUnit + widthSubUnit, heigthUnit);
+    ctx.lineTo(widthUnit, heigthUnit - widthSubUnit*2);
+    ctx.closePath();
+    ctx.fill();
 
     ctx.beginPath();
-    ctx.moveTo(25, 25);
-    ctx.lineTo(105, 25);
-    ctx.lineTo(25, 105);
+    ctx.moveTo(widthUnit * 7, heigthUnit * 7 - heigthUnit / 8);
+    ctx.lineTo(widthUnit * 7, heigthUnit * 7 + heigthUnit / 8);
+    ctx.lineTo(widthUnit * 7 + widthSubUnit * 2, heigthUnit * 7);
+    ctx.closePath();
     ctx.fill();
+
+    // Nombres ejes
+    ctx.font = `${widthSubUnit*3}px sans-serif`;
+    ctx.fillText("P", widthUnit - widthSubUnit*4, heigthUnit - heigthSubUnit);
+    ctx.fillText("Q", widthUnit*7 + widthSubUnit, heigthUnit*7 + heigthSubUnit*4);
+
+    // Curvas de demanda
+    ctx.strokeStyle = "rgb(229, 45, 35)";
+    ctx.beginPath();
+    ctx.moveTo(widthUnit, heigthUnit * 2);
+    ctx.lineTo(widthUnit * 6, heigthUnit * 7);
+    ctx.stroke();
+
+    ctx.strokeStyle = "rgb(0, 130, 252)";
+    ctx.beginPath();
+    ctx.moveTo(widthUnit, heigthUnit * 3);
+    ctx.lineTo(widthUnit * 5, heigthUnit * 7);
+    ctx.stroke();
+
+    // Intersección con ejes
+
+    // --------------  Solución  --------------- //
+    // Costo Marginal
+
+    // Precio Cliente Demanda Baja
+
+    // Intersección con eje Y
+
+
+
+
 }
 
 
